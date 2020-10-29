92b98f51b3c0d57fb328a4c0c16e3d2d620f424d
Weilun Hsia <wlhsia94@gmail.com>
2020-10-29 18:37:11 +0800

    201029

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import uuid
 import cv2
 import numpy as np
 import pandas as pd
-# from pdf2image import convert_from_path
+from pdf2image import convert_from_path
 from sklearn.cluster import KMeans
 from openpyxl import Workbook, load_workbook
 from openpyxl.styles import Font, Alignment, Border, Side
@@ -27,6 +27,7 @@ from io import StringIO
 import sqlite3
 import imagehash
 
+from unet import *
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -138,6 +139,22 @@ def delimg(img):
     return jsonify(response_object)
 
 
+@app.route('/uploadrecord', methods=['GET'])
+def uploadrecord():
+    response_object = {'status': 'success'}
+    if request.method == 'GET':
+        cu = cx.cursor()
+        cu.execute("select time, fileName, uploadUser from 'UploadRecord'")
+        data = cu.fetchall()
+        cu.close()
+        cx.commit()
+        list = []
+        for d in data:
+            list.append({"time": d[0], "fileName": d[1], "uploadUser": d[2]})
+        response_object['uploadRecordList'] = list
+    return jsonify(response_object)
+
+
 @app.route('/api/upload', methods=['POST'])
 def upload():
     response = {"success": False}
@@ -203,9 +220,9 @@ def updatedb():
     dcit_request = request.get_json()
     username = dcit_request['username']
     cu = cx.cursor()
-    for img in dcit_request['imgs']:
-        cu.execute("insert into Image values(null, '" + img + "', '" +
-                   username + "') WHERE imageName NOT IN (SELECT imageName FROM Image)")
+    for imgData in dcit_request['imgsData']:
+        cu.execute("insert into Image values(null, '" +
+                   imgData['imgName'] + "', '" + imgData['pHash'] + "', '" + imgData['group'] + "')")
     cu.close()
     cx.commit()
     response = {"success": True}
@@ -240,53 +257,115 @@ def compare():
             getExcelImgs(folderPath, file, imgsPath)
             shutil.move(os.path.join(folderPath, file),
                         os.path.join(excelsPath, file))
+        elif str(file).split('.')[-1].lower() == 'pdf':
+            getPDFImgs(folderPath, file, imgsPath)
+            shutil.move(os.path.join(folderPath, file),
+                        os.path.join(pdfsPath, file))
         fileName = str(file).split('_')[0]
         if fileName not in filesName:
             filesName.append(fileName)
     shutil.rmtree(folderPath)
 
-    # 上傳相片倆倆比對
+    imgsDataDF = pd.DataFrame(
+        columns=['imageName', 'pHash', 'fingerprint', 'group'])
+    # 將相片轉為特徵
     imgsName = os.listdir(imgsPath)
+    imgsData = {}
+    for imgName in imgsName:
+        img = Image.open(os.path.join(imgsPath, imgName))
+        pHash = phash(img)
+        fingerprint = [bool(int(d)) for d in str(bin(pHash))[2:].zfill(1024)]
+        imgsDataDF = imgsDataDF.append(
+            {'imageName': imgName, 'pHash': pHash, 'fingerprint': fingerprint}, ignore_index=True)
+        shutil.move(os.path.join(imgsPath, imgName),
+                    os.path.join(rawImgsPath, imgName))
+    shutil.rmtree(imgsPath)
+
+    # 將上傳相片分群
+    kmeans = joblib.load(os.path.join('./models', 'KMeans_model.m'))
+    pred = kmeans.predict(list(imgsDataDF.fingerprint))
+    imgsDataDF['group'] = pred
+
+    imgsDataDict = imgsDataDF.set_index('imageName').T.to_dict('list')
+
+    # 上傳相片倆倆比對
     result1 = []
     duplicateImgs = []
-    for i, (imgName1, imgName2) in enumerate(itertools.combinations(imgsName, 2)):
-        imghash1 = imagehash.phash(Image.open(
-            os.path.join(imgsPath, imgName1)))
-        imghash2 = imagehash.phash(Image.open(
-            os.path.join(imgsPath, imgName2)))
-        similary = 1 - (imghash1 - imghash2)/len(imghash1.hash)**2
+    for imgName1, imgName2 in itertools.combinations(imgsName, 2):
+        pHash1 = imgsDataDict[imgName1][0]
+        pHash2 = imgsDataDict[imgName2][0]
+        distance = bin(pHash1 ^ pHash2).count('1')
+        similary = 1 - distance / 1024
         if similary > 0.9:
             if imgName1 not in duplicateImgs:
                 result1.append(
                     {'imgName1': imgName1, 'imgName2': imgName2, 'similary': similary})
                 duplicateImgs.append(imgName2)
+    # imgsName = os.listdir(imgsPath)
+    # result1 = []
+    # duplicateImgs = []
+    # for i, (imgName1, imgName2) in enumerate(itertools.combinations(imgsName, 2)):
+    #     imghash1 = imagehash.phash(Image.open(
+    #         os.path.join(imgsPath, imgName1)))
+    #     imghash2 = imagehash.phash(Image.open(
+    #         os.path.join(imgsPath, imgName2)))
+    #     similary = 1 - (imghash1 - imghash2)/len(imghash1.hash)**2
+    #     if similary > 0.9:
+    #         if imgName1 not in duplicateImgs:
+    #             result1.append(
+    #                 {'imgName1': imgName1, 'imgName2': imgName2, 'similary': similary})
+    #             duplicateImgs.append(imgName2)
 
-    for imgName in imgsName:
-        shutil.move(os.path.join(imgsPath, imgName),
-                    os.path.join(rawImgsPath, imgName))
-    shutil.rmtree(imgsPath)
+    # for imgName in imgsName:
+    #     shutil.move(os.path.join(imgsPath, imgName),
+    #                 os.path.join(rawImgsPath, imgName))
+    # shutil.rmtree(imgsPath)
 
     # 上傳相片與資料庫相片比對
     result2 = []
     cu = cx.cursor()
-    cu.execute("select imageName from 'Image'")
-    data = cu.fetchall()
+    for group in set(imgsDataDF.group):
+        imgsNm = imgsDataDF.query("group == {}".format(group)).imageName
+        # cu.execute(
+        #     "select imageName, pHash, group from Image WHERE group like '" + str(group) + "'")
+        cu.execute("select imageName, pHash from Image")
+        data = cu.fetchall()
+        dbImages = []
+        for d in data:
+            dbImages.append([d[0], d[1]])
+        for imgName1 in imgsNm:
+            pHash1 = imgsDataDict[imgName1][0]
+            for dbImage in dbImages:
+                imgName2 = dbImage[0]
+                pHash2 = int(dbImage[1])
+                distance = bin(pHash1 ^ pHash2).count('1')
+                similary = 1 - distance / 1024
+                if similary > 0.9:
+                    result2.append(
+                        {'imgName1': imgName1, 'imgName2': imgName2, 'similary': similary})
+                    if imgName1 not in duplicateImgs:
+                        duplicateImgs.append(imgName1)
     cu.close()
-    dbImgsName = []
-    for d in data:
-        dbImgsName.append(d[0])
-    for imgName1 in imgsName:
-        imghash1 = imagehash.phash(Image.open(
-            os.path.join(rawImgsPath, imgName1)))
-        for imgName2 in dbImgsName:
-            imghash2 = imagehash.phash(Image.open(
-                os.path.join(rawImgsPath, imgName2)))
-            similary = 1 - (imghash1 - imghash2)/len(imghash1.hash)**2
-            if similary > 0.9:
-                result2.append(
-                    {'imgName1': imgName1, 'imgName2': imgName2, 'similary': similary})
-                if imgName1 not in duplicateImgs:
-                    duplicateImgs.append(imgName1)
+    # result2 = []
+    # cu = cx.cursor()
+    # cu.execute("select imageName from 'Image'")
+    # data = cu.fetchall()
+    # cu.close()
+    # dbImgsName = []
+    # for d in data:
+    #     dbImgsName.append(d[0])
+    # for imgName1 in imgsName:
+    #     imghash1 = imagehash.phash(Image.open(
+    #         os.path.join(rawImgsPath, imgName1)))
+    #     for imgName2 in dbImgsName:
+    #         imghash2 = imagehash.phash(Image.open(
+    #             os.path.join(rawImgsPath, imgName2)))
+    #         similary = 1 - (imghash1 - imghash2)/len(imghash1.hash)**2
+    #         if similary > 0.9:
+    #             result2.append(
+    #                 {'imgName1': imgName1, 'imgName2': imgName2, 'similary': similary})
+    #             if imgName1 not in duplicateImgs:
+    #                 duplicateImgs.append(imgName1)
 
     wb = Workbook()
     wb.create_sheet("工作表1", 0)
@@ -391,6 +470,13 @@ def compare():
     s2 = set(duplicateImgs)
     nonDuplicateImgs = list(s1.symmetric_difference(s2))
 
+    nonDuplicateImgsData = []
+    for nonDuplicateImg in nonDuplicateImgs:
+        pHash = str(imgsDataDict[nonDuplicateImg][0])
+        group = str(imgsDataDict[nonDuplicateImg][2])
+        nonDuplicateImgsData.append(
+            {'imgName': nonDuplicateImg, 'pHash': pHash, 'group': group})
+
     cu = cx.cursor()
     for file in files:
         cu.execute("insert into UploadRecord values(null, '" + datetime.now().strftime(
@@ -401,11 +487,20 @@ def compare():
     response = {
         "success": True,
         "result1": result1,
-        "result2": result2, "message": message,
-        "nonDuplicateImgs": nonDuplicateImgs,
+        "result2": result2,
+        "message": message,
+        "nonDuplicateImgsData": nonDuplicateImgsData,
         'resultFileName': resultFileName
     }
     return jsonify(response)
+
+
+def phash(img):
+    img = img.resize((32, 32), Image.ANTIALIAS).convert('L')
+    avg = reduce(lambda x, y: x + y, img.getdata()) / 1024.
+    hash_value = reduce(lambda x, y: x | (y[1] << y[0]), enumerate(
+        map(lambda i: 0 if i < avg else 1, img.getdata())), 0)
+    return hash_value
 
 
 def getWordImgs(folderPath, file, imgsPath):
@@ -446,6 +541,102 @@ def getExcelImgs(folderPath, file, imgsPath):
             img = Image.open(image.ref)
             imgFileName = '_'.join([file, sheet, str(i+1)])+'.jpg'
             img.save(os.path.join(imgsPath, imgFileName))
+
+
+graph = tf.get_default_graph()
+model = unet()
+model.load_weights(os.path.join('./models', 'unet.hdf5'))
+
+
+def getPDFImgs(folderPath, file, imgsPath):
+    with tempfile.TemporaryDirectory(dir='D:/temp') as path:
+        pageImgs = convert_from_path(os.path.join(
+            folderPath, file), output_folder=path, dpi=600)
+        for pageNumber, pageImg in enumerate(pageImgs):
+            if pageImg.size[0] < pageImg.size[1]:
+                pageImg = pageImg.rotate(90, Image.NEAREST, expand=True)
+            pageImg = np.array(pageImg)
+            rgb = cv2.cvtColor(pageImg, cv2.COLOR_BGR2RGB)
+            hsv = cv2.cvtColor(pageImg, cv2.COLOR_BGR2HSV)
+            gray = cv2.cvtColor(pageImg, cv2.COLOR_BGR2GRAY)
+            # unet
+            img = cv2.resize(gray, (256, 256))
+            img = np.reshape(img, img.shape+(1,)) if (not False) else img
+            img = np.reshape(img, (1,)+img.shape)
+            with graph.as_default():
+                result = model.predict(img)
+            result = result[0]
+            img = labelVisualize(
+                2, COLOR_DICT, result) if False else result[:, :, 0]
+            img = cv2.resize(img, (pageImg.shape[1], pageImg.shape[0]))
+            img = (img*255).astype(np.uint8)
+            (thresh, im_bw) = cv2.threshold(img, 0.05, 255, 0)
+            contours, hierarchy = cv2.findContours(
+                im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            unet_conts = [
+                contour for contour in contours if cv2.contourArea(contour) > 500000]
+            # sat
+            # 取出飽和度
+            saturation = hsv[:, :, 1]
+            _, threshold = cv2.threshold(
+                saturation, 1, 255.0, cv2.THRESH_BINARY)
+            # 2值化圖去除雜訊
+            kernel_radius = int(threshold.shape[1]/300)
+            kernel = np.ones((kernel_radius, kernel_radius), np.uint8)
+            threshold = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+            # 產生等高線
+            contours, hierarchy = cv2.findContours(
+                threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            sat_conts = [
+                contour for contour in contours if cv2.contourArea(contour) > 500000]
+
+            if len(unet_conts) == 6:
+                conts = unet_conts
+            elif len(sat_conts) == 6:
+                conts = sat_conts
+            elif len(sat_conts) > len(unet_conts):
+                conts = sat_conts
+            else:
+                conts = unet_conts
+
+            sortY_conts = sorted([cont for cont in conts],
+                                 key=lambda x: x[0][0][1], reverse=False)
+            up_conts = sortY_conts[:3]
+            up_conts = sorted([cont for cont in up_conts],
+                              key=lambda x: x[0][0][0], reverse=False)
+            down_conts = sortY_conts[3:]
+            down_conts = sorted([cont for cont in down_conts],
+                                key=lambda x: x[0][0][0], reverse=False)
+            merge_conts = up_conts+down_conts
+
+            for i, c in enumerate(merge_conts):
+                # 嘗試在各種角度，以最小的方框包住面積最大的等高線區域，以紅色線條標示
+                rect = cv2.minAreaRect(c)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                angle = rect[2]
+                if angle < -45:
+                    angle = 90 + angle
+                # 以影像中心為旋轉軸心
+                (h, w) = pageImg.shape[:2]
+                center = (w // 2, h // 2)
+                # 計算旋轉矩陣
+                M = cv2.getRotationMatrix2D(center, angle, 1.0)
+                # 旋轉圖片
+                rotated = cv2.warpAffine(
+                    rgb, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT)
+                # 旋轉紅色方框座標
+                pts = np.int0(cv2.transform(np.array([box]), M))[0]
+                #  計算旋轉後的紅色方框範圍
+                y_min = min(pts[0][0], pts[1][0], pts[2][0], pts[3][0])
+                y_max = max(pts[0][0], pts[1][0], pts[2][0], pts[3][0])
+                x_min = min(pts[0][1], pts[1][1], pts[2][1], pts[3][1])
+                x_max = max(pts[0][1], pts[1][1], pts[2][1], pts[3][1])
+                # 裁切影像
+                img_crop = rotated[x_min:x_max, y_min:y_max]
+                page_num = (str(pageNumber+1)).zfill(3)
+                dst_filenm = '_'.join([file, 'Page'+page_num, str(i+1)])+'.jpg'
+                cv2.imwrite(os.path.join(imgsPath, dst_filenm), img_crop)
 
 
 app.run()
